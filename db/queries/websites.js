beca4fc4974ecf2f2e78ce3bc57674cb50598078
Lyon <lyon.b.lee@gmail.com>
2023-01-04 13:42:04 -0500

    temporary commit to pull

--- db/queries/websites.js
+++ db/queries/websites.js
@@ -1,5 +1,6 @@
 const db = require('../connection');
-// Hello
+
+// Add new website and its password - NOT IMPLEMENTED YET
 const addWebsites = function(website_name, url, category, password, user_id, organization_id){
   return db
   .query(`INSERT INTO websites (website_name, url, category, password, user_id, organization_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`, [website_name, url, category, password, user_id, organization_id])
@@ -8,33 +9,36 @@ const addWebsites = function(website_name, url, category, password, user_id, org
   })
 }
 
+// Get all websites that are available to the specified user
 const getWebsites = function(user_id){
   return db
-  .query(`SELECT * FROM websites WHERE user_id = $1`, [user_id])
+  .query(`SELECT * FROM passwords WHERE user_id = $1`, [user_id])
   .then((result) => {
     return result.rows;
   })
 }
 
+// Get website based on its id
 const getWebsiteById = function(id){
   return db
-  .query(`SELECT * FROM websites WHERE id = $1`, [id])
+  .query(`SELECT * FROM passwords WHERE id = $1`, [id])
   .then((result) => {
     return result.rows[0];
   })
 }
 
+// Update password to the website
 const updatePassword = function(newPassword, id) {
   return db
-  .query(`UPDATE websites SET password = $1 WHERE id = $2`, [newPassword, id])
+  .query(`UPDATE passwords SET password = $1 WHERE id = $2`, [newPassword, id])
 }
 
+// Delete the website from the passwords database
 const deleteWebsite = function(id) {
   return db
-  .query(`DELETE FROM websites WHERE id = $1`, [id])
+  .query(`DELETE FROM passwords WHERE id = $1`, [id])
 }
 
- // Missing Create and Read
 module.exports = {
   addWebsites,
   getWebsites,
